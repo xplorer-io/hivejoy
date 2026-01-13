7517725e41484febe03455243e344cba492508bb
Adarsha Aryal <adarsha.aryal653@gmail.com>
2026-01-13 22:34:54 +1100

    Build Fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -20,7 +20,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     async session({ session, token }) {
       if (session.user) {
         session.user.id = token.id as string
-        session.user.role = token.role as string
+        session.user.role = token.role as UserRole
       }
       return session
     },
